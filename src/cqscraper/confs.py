d13b4e53ab103127554951be8e56dad052399390
Sergi Cortés <sergi_cortes@hotmail.com>
2024-05-26 19:29:49 +0200

    feat(src): add confs to centralize some configurations

---
+++ src/cqscraper/confs.py
@@ -0,0 +1,5 @@
+PRODUCTS_URL = "https://www.larodan.com/products/category/monounsaturated-fa/"
+PRODUCT_LINK_CLASS = "woocommerce-loop-product__link"
+PRODUCT_URL_NEXT = "/page/"
+
+FILE_NAME_PRODUCTS_RESULTS = "products_results.json"
